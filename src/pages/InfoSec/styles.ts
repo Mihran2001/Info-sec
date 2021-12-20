c4c7568a5aa5659285f8374bb61540a3903b983e
Mihran <xachatryan200104@gmail.com>
2021-12-21 00:07:06 +0400

    fixes

--- src/pages/InfoSec/styles.ts
+++ src/pages/InfoSec/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const HeroSection = styled.section`
   position: relative;
-  height: 100vh;
+  height: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
